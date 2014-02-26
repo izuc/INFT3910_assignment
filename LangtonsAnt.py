3a3f97cd656c22f08a7ebfc2eb8eefd8fda67bf2
Lance Baker <the@lance.name>
2014-02-26 21:55:28 +1100

    Latest

--- LangtonsAnt.py
+++ LangtonsAnt.py
@@ -1,10 +1,17 @@
+#Langton's Ant - INFT3910 Assignment 3
+#Created 28/04/2010
+#Author: Lance Baker
+
+#Import packages
 from Tkinter import *
+import tkFileDialog
 import pylab as py
 import sqlite3 as sql
 from PIL import Image, ImageTk, ImageGrab
 import os.path
 import time
 
+#Class that holds the variables for the ant direction (Compass)
 class Compass:
     North = 1
     South = 2
@@ -32,13 +39,16 @@ class Ant:
         return position
         
     def flipState(self):
-        self.posy = self.checkBoundary(self.posy)
-        self.posx = self.checkBoundary(self.posx)
-        index = (self.grid.gridpoints[self.posy][self.posx][2] + 1) % (len(self.ruleString))
-        rule = self.ruleString[index]
-        self.grid.gridpoints[self.posy][self.posx][2] = index
-        self.grid.gridpoints[self.posy][self.posx][0] = rule[0]
-        self.grid.itemconfigure(self.grid.gridpoints[self.posy][self.posx][1], fill=rule[1]) 
+        try:
+            self.posy = self.checkBoundary(self.posy)
+            self.posx = self.checkBoundary(self.posx)
+            index = (self.grid.gridpoints[self.posy][self.posx][2] + 1) % (len(self.ruleString))
+            rule = self.ruleString[index]
+            self.grid.gridpoints[self.posy][self.posx][2] = index
+            self.grid.gridpoints[self.posy][self.posx][0] = rule[0]
+            self.grid.itemconfigure(self.grid.gridpoints[self.posy][self.posx][1], fill=rule[1])
+        except:
+            print("")
 
     def setDirAndMove(self):
         def move(position, rule):
@@ -63,7 +73,8 @@ class Ant:
             self.posy = move(self.posy, not rule)
         self.flipState()
         self.steps += 1
-        
+
+#Class that has our control variables       
 class Control:
     Start=1
     Running=2
@@ -79,6 +90,7 @@ class Grid(Canvas):
         self.ant = Ant(self)
         self.application = application
         self.pack(fill=BOTH)
+        #self.db.getCurrentRun()
         
     def initGridPoints(self):
         self.gridpoints = []
@@ -115,7 +127,7 @@ class Grid(Canvas):
                 self.drawGrid()
                 self.state = Control.Running
                 self.application.db.newRun()
-				
+            #stepLimit = 0
             while (self.state == Control.Running): 
                 self.ant.setDirAndMove()
                 self.update()
@@ -125,8 +137,7 @@ class Grid(Canvas):
                     Application.strAntPos.set(self.gridpoints[self.ant.posy][self.ant.posx][3])
                     Application.strTimeSteps.set(self.ant.steps)
                 except Exception, e:
-                    print e
-                    
+                    print e  
         except:
             print("Application Ended")
             print(sys.exc_info()[0])
@@ -135,25 +146,31 @@ class Grid(Canvas):
 class Graph:
     strLineFormat = ""
     def __init__(self):
-        strLineFormat = "-"
+        Graph.strLineFormat = "-"
 
     def setTitles(self, strTitle, strXName, strYName):
+        #Sets graph labels
         py.xlabel(strXName)
         py.ylabel(strYName)
         py.title(strTitle)
-        
-    def plotLines(self, lstPlots):
+
+    #plots the runs onto the graph
+    def plotLines(self, lstPlots, lstSelected):
         intCounter = 1
+        #loops through list and adds runs to the plot
         for plot in lstPlots:
             py.plot(len(plot),10,plot, "-",label='run ' + str(intCounter))
             intCounter +=1
-                 
+            
+        #strLegend = str(lstSelected)
+        py.legend(lstSelected)
+    #Sets the line           
     def setLineFormat(self, strLine):
         if(strLine != ""):
-            strLineFormat = strLine
+            Graph.strLineFormat = strLine
             
     def returnLineFormat(self):
-        return strLineFormat
+        return Graph.strLineFormat
             
     def showGraph(self):
         py.show()
@@ -259,7 +276,7 @@ class Database:
         self.currentRun = None
 
     def createDB(self):
-        #Need to run this if the DB doesn't exist.
+        #Need to run this if the DB doesn't exsist.
         self.cur.execute('CREATE TABLE IF NOT EXISTS runs(runId INTEGER PRIMARY KEY AUTOINCREMENT, run_date DATETIME)')
         self.cur.execute('CREATE TABLE IF NOT EXISTS ant(id INTEGER PRIMARY KEY AUTOINCREMENT, runId INTEGER, step INTEGER, pos INTEGER)')
 
@@ -271,7 +288,7 @@ class Database:
         #self.conn.commit()
 
     def insert(self, strings):
-        #No longer used, but might be useful later for inserting
+        #No longer used, but might be useful latr for inserting
         #stuff into the DB.
         for q in strings:
             self.cur.execute(q)
@@ -282,7 +299,8 @@ class Database:
         self.conn.commit()
 
     def newRun(self):
-        #Sets the value for the next run
+        #Sets the calue for the next run
+        #newRun =  + 1
         self.cur.execute('INSERT INTO runs(run_date) VALUES(DATETIME(\'NOW\'))')
         self.cur.execute('SELECT runId FROM runs ORDER BY runId DESC LIMIT 1')
         for data in self.cur: 
@@ -308,6 +326,7 @@ class Database:
 class Application(Frame):
     strAntPos = ""
     strTimeSteps = ""
+    
     def __init__(self, master):
         Frame.__init__(self, master, width=650, height=650)
         self.grid = Grid(self, 100, 100)
@@ -315,20 +334,27 @@ class Application(Frame):
         self.createToolbar(master)
         self.createRightToolbar(master)
         self.createTopToolbar(master)
-
         self.pack()
 
     def showRules(self):
         window = Toplevel()
         window.title('Rule Settings')
         
-        Label(window, text="Colour: ", width=5, anchor=W).grid(row=0, column=0, sticky=W)
+        Label(window, text="Colour: ", width=5, anchor=W).grid(row=0, column=0, sticky=NW)
         txtColour = Entry(window, name="colour", width=25)
-        txtColour.grid(row=0, column=1, sticky=E)
+        listbox = Listbox(window, width=25, height=6)
+        for item in ["white", "black", "blue", "red", "pink", "green"]:
+            listbox.insert(END, item)
 
+        listbox.grid(row=0, column=1, sticky=E)
+
+        ruleInt = IntVar()        
         Label(window, text="Rule: ", width=5, anchor=W).grid(row=1, column=0, sticky=W)
-        txtRule = Entry(window, name="rule", width=25)
-        txtRule.grid(row=1, column=1, sticky=E)
+        R1 = Radiobutton(window, text="Left", variable=ruleInt, value=0)
+        R1.grid(row=1, column=1, sticky=W)
+        R2 = Radiobutton(window, text="Right", variable=ruleInt, value=1)
+        R2.grid(row=1, column=1, sticky=E)
+        
         self.rowSelected = 0
 
         def rowSelectAction(row):
@@ -339,11 +365,17 @@ class Application(Frame):
         
         
         def addRule():
-            grid.insert(END, (len(self.grid.ant.ruleString), txtColour.get().strip(), int(txtRule.get().strip())))
-            self.grid.ant.ruleString.append([int(txtRule.get().strip()), txtColour.get().strip()])
-
+            try:
+                colour = listbox.get(listbox.curselection()[0]) 
+                grid.insert(END, (len(self.grid.ant.ruleString), colour, ruleInt.get()))
+                self.stateGrid.insert(END, (colour, ruleInt.get()))
+                self.grid.ant.ruleString.append([ruleInt.get(), colour])
+            except:
+                print("You must select a value first")
+            
         def removeRule():
             grid.delete(self.rowSelected)
+            self.stateGrid.delete(self.rowSelected)
             del self.grid.ant.ruleString[self.rowSelected]
             
         
@@ -360,48 +392,105 @@ class Application(Frame):
         btnRemove.grid(row=3, column=1, sticky=E)
         
     def showGraph(self):
+        lstSelectedGraphs = []
         window = Toplevel()
         window.title('History')
         run_data = self.db.getRuns()
         
         self.rowSelected = 0
-        print "run data", len(run_data)
         
         def displaySelectedGraph():
-            print "row selected", self.rowSelected
+            #Checks to see if our list is empty, if so, use the selected row (meaning only one selected)
+            if(len(lstSelectedGraphs) == 0):
+                lstSelectedGraphs.append(self.rowSelected+1)
+                
+            #Checks to see if we have any data
             if (len(run_data) > 0):
                 gr = Graph()
                 gr.setTitles('History Comparison', 'Time Steps', 'Ant Position')
                 
-                lstPlots = []            
-                lstPlots.append(self.db.returnRun(run_data[self.rowSelected][0]))
+                lstPlots = []
 
-                gr.plotLines(lstPlots)
+                #loops through all the runs and compiles another list of run results
+                for item in lstSelectedGraphs:    
+                    lstPlots.append(self.db.returnRun(item))
+
+                #Sorts list
+                lstSelectedGraphs.sort()
+                #Displays graph
+                print "Displaying Graphs for runs: " + str(lstSelectedGraphs)
+                gr.plotLines(lstPlots, lstSelectedGraphs)
                 gr.showGraph()
-            
+
+        #Sets the selected row
         def rowSelectAction(row):
             self.rowSelected = row
-            
-        grid = DataGrid(window, (('Run #', 10), ('Date Time', 50)), rowSelectAction)
-        grid.grid(row=1, column=0, columnspan=2)
 
+        def addRun():
+            #Boolean used to flag graphs already selected
+            blnFound = False
+            #Checks to see if this is the first value or not
+            if(len(lstSelectedGraphs) > 0):
+                #loops through items in our list
+                for item in lstSelectedGraphs:
+                    #Checks to see if it has found the selected value in the list
+                    if(int(item) == int(run_data[self.rowSelected][0])):
+                        blnFound = True
+                #if its not found, then add the run to the list
+                if(not blnFound):        
+                    lstSelectedGraphs.append(run_data[self.rowSelected][0])
+                    strSelected.set(str(lstSelectedGraphs))
+            else:
+                lstSelectedGraphs.append(run_data[self.rowSelected][0])
+                strSelected.set(str(lstSelectedGraphs))
+
+        def removeRun():
+            del lstSelectedGraphs[-1:]
+            strSelected.set(str(lstSelectedGraphs))
+            
+        #Creates labels and our data grid            
+        labelHeading = Label(window, anchor="w",text="Here you can select one or more runs to display a graph on.\nTo select one graph, simply highlight the run and click 'Display'.\nTo select multiple graphs, select a run and hit 'Add', continue\nuntil all graphs have been selected, then click 'Display'")
+        labelHeading.grid(row=0, columnspan=2, sticky=W+N+E+S)    
+        lblSpace = Label(window, anchor="ne", width=10)
+        lblSpace.grid(row=1, columnspan=2, sticky=W+N)
+        grid = DataGrid(window, (('Run #', 10), ('Date Time', 50)), rowSelectAction)
+        grid.grid(row=2, column=0, columnspan=3)
+
+        #Inserts the run data into the data grid
         for result in run_data:
             grid.insert(END, (result[0], result[1]))
 
-        btnDisplay = Button(window, text="Display", width=10, command=displaySelectedGraph)
-        btnClose = Button(window, text="Close", width=10, command=window.destroy)
-        btnDisplay.grid(row=2, column=0,padx=10,pady= 10, sticky=W)
-        btnClose.grid(row=2, column=1, padx=10,pady=10, sticky=W)
+        #initialises our text variable for selected graphs
+        strSelected = StringVar()
+        strSelected.set("")
+
+        #Creates all the buttons and labels
         
+        lblSelectGraph = Label(window, text="Selected Graphs:",anchor="w")
+        lblSelectGraph.grid(row=3, column=1, rowspan=2, sticky=W)
+        lblSelected = Label(window, textvariable=strSelected,anchor="w")
+        lblSelected.grid(row=3, column=2, rowspan=2, sticky=W)
+        btnSelect = Button(window, text="Add", width=10, command=addRun)
+        btnSelect.grid(row=3, column=0, padx=10,pady=10, sticky=W)
+        btnRemove = Button(window, text="Remove", width=10, command=removeRun)
+        btnRemove.grid(row=4, column=0, padx=10,pady=5, sticky=W)
+        btnDisplay = Button(window, text="Display", width=10, command=displaySelectedGraph)
+        btnDisplay.grid(row=5, column=0,padx=10,pady= 10, sticky=W)
+        btnClose = Button(window, text="Close", width=10, command=window.destroy)
+        btnClose.grid(row=5, column=1, columnspan=2, padx=10,pady=10, sticky=W)
+            
     def showAbout(self):
         about = Toplevel()
         about.title('About')
+        #Creates string variable for our content label
         strContent = StringVar()
         strContent.set('Content')
         labelHeading = Label(about, text="About", width=30).grid(row=0, sticky=W+N+E+S)
         labelContent = Label(about, textvariable=strContent).grid(row=1, sticky=W+N, padx=10)
-        strContent.set("This application was designed and developed \nby Lance Baker for INFT3910 Assignment 3.")
-        
+        strContent.set("This application was developed for INFT3910 Assignment 3.")
+        lblMembers = Label(about, text="By Lance Baker 2010").grid(pady=10, row=2, column=0, columnspan=3,sticky=W+N+E)
+
+        #Creates buttons and the logo image
         logoImage = Image.open("Ant.jpg")
         size = 100, 100
         logoImage.thumbnail(size)
@@ -414,6 +503,7 @@ class Application(Frame):
         btnClose.grid(row=3, column=3, pady=10, sticky=W)
         
     def createToolbar(self, master):
+        #Creates bottom toolbar with our control buttons
         self.toolbar = Frame(master)
         Button(self.toolbar, text="Start", width=10, command=self.grid.start).pack(side=LEFT, padx=2, pady=2)
         Button(self.toolbar, text="Stop", width=10, command=self.grid.stop).pack(side=LEFT, padx=2, pady=2)
@@ -422,19 +512,21 @@ class Application(Frame):
         self.toolbar.pack(side=BOTTOM, fill=X)
 
     def createRightToolbar(self, master):
-        self.toolbar = Frame(master)
-       
-        self.rowSelected = 0
+        master.toolbar = Frame(master)
+        master.update()
         
+        #Creates label frame for our controls
         group = LabelFrame(master, text="Details", padx=5, pady=5)
         group.grid(row=1, columnspan=2)
         group.pack(padx=10, pady=10, side=RIGHT,anchor="nw")
-        
+
+        #Initialises the text variables for the labels
         Application.strAntPos = StringVar()
         Application.strAntPos.set("")
         Application.strTimeSteps = StringVar()
         Application.strTimeSteps.set("")
-        
+
+        #Creates relevant labels
         lblTime = Label(group, text="Time steps:", anchor="ne", width=10).grid(row=0, column=0, sticky=W+N)
         lblPostion = Label(group, text="Postition:", anchor="ne", width=10).grid(row=1, column=0, sticky=W+N)
 
@@ -443,20 +535,49 @@ class Application(Frame):
         lblSpace = Label(group, anchor="ne", width=10).grid(row=3, column=0, sticky=W+N)
         lblRule = Label(group, text="Rules:", anchor="w", width=10).grid(row=4, column=0, sticky=W+N)
 
+        #Sets the row selected
         def rowSelectAction(row):
             self.rowSelected = row
             
-        grid = DataGrid(group, (('Colour', 10), ('Rule', 10)), rowSelectAction)
-        grid.grid(row=5, column=0, columnspan=2, sticky=W+E)
+        #Instantiates the data grid and its location on the grid    
+        self.stateGrid = DataGrid(group, (('Colour', 10), ('Rule', 10)), rowSelectAction)
+        self.stateGrid.grid(row=5, column=0, columnspan=2, sticky=W+E)
 
+        #inserts values into the data grid
         for index, cell in enumerate(self.grid.ant.ruleString):
-            grid.insert(END, (cell[1], cell[0]))
+            self.stateGrid.insert(END, (cell[1], cell[0]))
             
-        self.toolbar.pack(side=RIGHT, fill=Y)
+        master.toolbar.pack(side=RIGHT, fill=Y)
+
+    #Function that captures a screenshot   
+    def captureScreenShot(self, master):
+        #Gets the save file info from the user
+        filename = tkFileDialog.asksaveasfilename(defaultextension="jpg")
+        #Updates master window
+        master.update()
+
+        #Gets location from the geometry string
+        strGeoInfo = str(master.winfo_geometry())
+        intIndexPlus1 = str.find(strGeoInfo, '+', 0, len(strGeoInfo))
+        strSub1 = strGeoInfo[intIndexPlus1 + 1:]
         
-    def captureScreenShot(self):
-        time.sleep(1)
-        ImageGrab.grab().save('box.png')
+        intIndexPlus2 = str.find(strSub1, '+', 0, len(strSub1))
+
+        strSub2 = strSub1[intIndexPlus2+1:]
+        strSub1 = strSub1[:-(intIndexPlus2+1)]
+        
+        if(filename != ""):
+            #Catch statement for any errors in the location found above
+            try:
+                #Updates the master form again
+                master.update()
+                #Grabs a screenshot and saves it to the directory and name from the save dialog above
+                ImageGrab.grab().save(filename)
+                print "Saved screenshot to: " + filename
+            except:
+                print "Screen size not in integer format."            
+        else:
+            print "Screenshot has been cancelled."
         
     def createTopToolbar(self, master):
         menubar = Menu(master)
@@ -471,7 +592,7 @@ class Application(Frame):
         controlsmenu.add_command(label="Start", command=self.grid.start) 
         controlsmenu.add_command(label="Stop", command=self.grid.stop)
         controlsmenu.add_separator()
-        controlsmenu.add_command(label="Save Image", command=self.captureScreenShot)
+        controlsmenu.add_command(label="Save Image", command=lambda i=master: self.captureScreenShot(i))
         menubar.add_cascade(label="Controls", menu=controlsmenu)
 
         #Add tools option
@@ -491,6 +612,9 @@ class Application(Frame):
 if __name__ == "__main__":
     root = Tk()
     root.title('Langton\'s Ant')
-
+    #Make window fixed size
+    root.resizable(width=FALSE, height=FALSE)
+    #Updates root window
+    root.update()
     toolbar = Frame(root)
     Application(root).mainloop()
